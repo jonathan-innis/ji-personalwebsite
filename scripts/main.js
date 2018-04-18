d0f225e5c430d29f609c9c9944ef994b9c403a59
Jonathan <jonathan.innis.ji@gmail.com>
2018-04-18 15:52:51 -0500

    Deleted snapscroll and changed font

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,9 @@ define(function (require) {
     var ProgressBar = require('progressbar');
     var scrollSnap = require('scrollSnap');
 
-    scrollSnap.init({
-        elements: document.querySelectorAll('section'),
-        minWidth: 600,
-        minHeight: 400,
-        detectMobile: true,
-        keyboard: true,
-        duration: 20,
-        timing: scrollSnap._timing
-    });
-
     window.sr = ScrollReveal();
+
+    sr.reveal(".header");
 
     sr.reveal('#progressBars', {
         afterReveal: function() {
@@ -27,16 +19,24 @@ define(function (require) {
                 duration: 800,
                 easing: 'easeInOut'
             });
+            progressBar3.animate(0.3, {
+                duration: 800,
+                easing: 'easeInOut'
+            });
         }
     });
 
 
     var progressBar1 = new ProgressBar.Line('#progress1', {
         strokeWidth: 2,
-        color: "#000"
+        color: "#F44336"
     });
     var progressBar2 = new ProgressBar.Line('#progress2', {
         strokeWidth: 2,
-        color: "#000"
+        color: "#F44336"
+    });
+    var progressBar3 = new ProgressBar.Line('#progress3', {
+        strokeWidth: 2,
+        color: "#F44336"
     });
 });
